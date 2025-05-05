502761f260eb39c8b89921e0a6f20e565839ab4d
gui-tr <gtrionfo@proton.me>
2025-05-05 09:20:21 +0100

    article-medium-width-def

--- wp-content/themes/sasexpliq-theme/functions.php
+++ wp-content/themes/sasexpliq-theme/functions.php
@@ -225,7 +225,7 @@ function sasexpliq_process_contact_form() {
     }
 
     // Email content
-    $to = get_option( 'admin_email' );
+    $to = 'info@sasexpliq.com';
     $subject = 'Nouveau message de contact de ' . $name;
     $body = "Nom: $name\n\nEmail: $email\n\nMessage:\n$message";
     $headers = array('Content-Type: text/plain; charset=UTF-8');
@@ -316,57 +316,16 @@ function sasexpliq_rename_theme_color_taxonomy() {
 }
 add_action('init', 'sasexpliq_rename_theme_color_taxonomy', 1000);
 
-// /**
-//  * Modify article post type to include theme in URL structure
-//  */
-// function sasexpliq_modify_article_permalink($post_link, $post) {
-//     if ($post->post_type === 'article') {
-//         // Get the theme color terms for this article
-//         $terms = get_the_terms($post->ID, 'theme_color');
-        
-//         if ($terms && !is_wp_error($terms)) {
-//             $theme_slug = $terms[0]->slug;
-            
-//             // Get the theme post that matches this term
-//             $theme_args = array(
-//                 'post_type' => 'theme',
-//                 'posts_per_page' => 1,
-//                 'tax_query' => array(
-//                     array(
-//                         'taxonomy' => 'theme_color',
-//                         'field' => 'slug',
-//                         'terms' => $theme_slug,
-//                     ),
-//                 ),
-//             );
-            
-//             $theme_query = new WP_Query($theme_args);
-//             if ($theme_query->have_posts()) {
-//                 $theme_query->the_post();
-//                 $theme_post_name = get_post_field('post_name', get_the_ID());
-//                 wp_reset_postdata();
-                
-//                 // Replace the "article" part with "theme/theme-name"
-//                 $post_link = str_replace('article/', 'theme/' . $theme_post_name . '/', $post_link);
-//             }
-//         }
-//     }
-    
-//     return $post_link;
-// }
-// add_filter('post_type_link', 'sasexpliq_modify_article_permalink', 10, 2);
-
-// /**
-//  * Add a rewrite rule to handle the new URL structure
-//  */
-// function sasexpliq_add_article_rewrite_rules() {
-//     add_rewrite_rule(
-//         'theme/([^/]+)/([^/]+)/?$',
-//         'index.php?post_type=article&name=$2',
-//         'top'
-//     );
-// }
-// add_action('init', 'sasexpliq_add_article_rewrite_rules');
+/**
+ * Limit excerpt length for articles
+ */
+function sasexpliq_custom_excerpt_length($length) {
+    if (get_post_type() === 'article') {
+        return 20; // Number of words
+    }
+    return $length;
+}
+add_filter('excerpt_length', 'sasexpliq_custom_excerpt_length');
 
 /**
  * Include additional files
