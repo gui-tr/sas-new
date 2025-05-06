266c24938279859ef0ee865395d1407c9c24bf6e
gui-tr <gtrionfo@proton.me>
2025-05-06 20:50:43 +0100

    update

--- wp-content/themes/sasexpliq-theme/footer.php
+++ wp-content/themes/sasexpliq-theme/footer.php
@@ -7,12 +7,15 @@
     <footer id="colophon" class="site-footer">
         <div class="container">
             <div class="footer-content">
-                <div class="footer-info">
-                    <p class="association-name">Association Sasexpliq</p>
-                    <p class="association-address">Chemin de la menuiserie 33<br>1293 Bellevue / GE</p>
-                    <p class="association-email"><a href="mailto:info@sasexpliq.com">info@sasexpliq.com</a></p>
+                <div class="footer-left">
+                    <p>Avec le soutien de</p>
+                    <div class="support-image">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/etat-geneve.jpg" alt="République et Canton de Genève" style="max-width:100px; height:auto;" />
+                    </div>
                 </div>
-                <div class="footer-social">
+                <div class="footer-right">
+                    <!-- <p class="association-name">Association Sasexpliq</p> -->
+                    <p class="association-email"><a href="mailto:info@sasexpliq.com">info@sasexpliq.com</a></p>
                     <a href="https://www.instagram.com/sasexpliq" target="_blank" rel="noopener noreferrer" class="social-link instagram">
                         <span class="screen-reader-text">Instagram</span>
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/ig-icon.png" alt="Instagram" width="32" height="32" />
